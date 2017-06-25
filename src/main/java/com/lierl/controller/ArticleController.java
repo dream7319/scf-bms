4320a75c032a1b1bb69a41ac03fc11c5eff1620c
lierl <lierlei0515@163.com>
2017-06-25 21:55:31 +0800

    update

--- src/main/java/com/lierl/controller/ArticleController.java
+++ src/main/java/com/lierl/controller/ArticleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,8 +32,13 @@ public class ArticleController {
 
     @GetMapping("/add")
     public Article addArticle(){
-        Article article = articleService.selectById(1);
-
+//        Article article = articleService.selectById(1);
+        Article article = new Article();
+        article.setTitle("测试");
+        article.setContent("我们建立一个网站或应用程序，并要添加搜索功能，但是想要完成搜索工作的创建是非常困难的。我们希望搜索解决方案要运行速度快，我们希望能有一个零配置和一个完全免费的搜索模式，我们希望能够简单地使用JSON通过HTTP来索引数据，我们希望我们的搜索服务器始终可用，我们希望能够从一台开始并扩展到数百台，我们要实时搜索，我们要简单的多租户，我们希望建立一个云的解决方案。因此我们利用Elasticsearch来解决所有这些问题以及可能出现的更多其它问题");
+        article.setAbstracts("添加搜索功能，但是想要完成搜索工作的创建是非");
+        article.setClickCount(200L);
+        article.setPostTime(new Date());
         articleRepository.save(article);
 
         return article;
@@ -40,7 +46,7 @@ public class ArticleController {
 
     @GetMapping("/{id}")
     public Article selectOne(@PathVariable Long id){
-        articleRepository.search()
+//        articleRepository.search()
         return articleRepository.findOne(id);
     }
 }
