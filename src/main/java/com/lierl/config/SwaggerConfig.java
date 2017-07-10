2255623a52d5e813e53526b35b7b68bd5d6dfea6
lierlei <lierlei@xingyoucai.com>
2017-07-10 10:02:53 +0800

    add generator

---
+++ src/main/java/com/lierl/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.lierl.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author lierl
+ * @create 2017-07-10 9:41
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.lierl"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("供应链金融 RESTful APIs")
+                .description("供应链金融")
+                .termsOfServiceUrl("https://github.com/dream7319")
+                .contact("lierlei0515@163.com")
+                .version("1.0")
+                .build();
+    }
+}
