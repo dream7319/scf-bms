27f1f5bc7c9a637646c3d068522dcad710e6c9d7
lierlei <lierlei@xingyoucai.com>
2017-07-11 13:41:46 +0800

    update

--- src/main/java/com/lierl/entity/User.java
+++ src/main/java/com/lierl/entity/User.java
@@ -1,11 +1,14 @@
 package com.lierl.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 
 /**
  * Created by lierl on 2017/6/25.
  */
 //@TableName("user")
+@Data
 public class User {
     private Integer id;
     private String username;//用户名
@@ -13,7 +16,7 @@ public class User {
     private Integer age;//年龄
     private String name;//用户别名
     private String sex;// MALE,FEMALE
-    private String email;//邮箱
+	private String email;//邮箱
     private String telphone;//电话
     private String photo;//头像
     private Boolean userStatus;//用户状态 0:禁用，1:可用
@@ -22,123 +25,4 @@ public class User {
     private Date createTime;//创建日期
     private Date updateTime;//更新日期
     private Boolean deleteFlag;//删除标识：0：删除,1：未删除
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Boolean getUserStatus() {
-        return userStatus;
-    }
-
-    public void setUserStatus(Boolean userStatus) {
-        this.userStatus = userStatus;
-    }
-
-    public Boolean getDeleteFlag() {
-        return deleteFlag;
-    }
-
-    public void setDeleteFlag(Boolean deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelphone() {
-        return telphone;
-    }
-
-    public void setTelphone(String telphone) {
-        this.telphone = telphone;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
-
-    public Boolean getUserType() {
-        return userType;
-    }
-
-    public void setUserType(Boolean userType) {
-        this.userType = userType;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
