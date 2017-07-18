660046e84a1d6a7d726bcf2cd9c2fecc8305fb3a
lierlei <lierlei@xingyoucai.com>
2017-07-18 18:10:09 +0800

    add

--- src/main/java/com/lierl/util/Utils.java
+++ src/main/java/com/lierl/util/Utils.java
@@ -1,25 +1,9 @@
 package com.lierl.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang.time.DateFormatUtils;
 
 public class Utils {
 
@@ -27,22 +11,6 @@ public class Utils {
 
 	private static final String HMAC_SHA256 = "HmacSHA256";
 
-	public static String getRandomFileName() {
-		String str = new SimpleDateFormat("yyyyMMdd").format(new Date());
-		Random random = new Random();
-		int rannum = (int) (random.nextDouble() * (99999 - 10000 + 1)) + 10000;// 获取5位随机数
-		return str + rannum;// 当前时间
-	}
-
-	public static String getFileType(String fileName) {
-		return fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
-	}
-
-	public static String getHashPath(long id) {
-		long first = id % 10;
-		long second = (id / 10) % 10;
-		return first + File.separator + second + File.separator;
-	}
 
 	public static String hashHmac(String data, String secret) {
 		try {
@@ -53,116 +21,5 @@ public class Utils {
 		} catch (Exception e) {
 			return null;
 		}
-	}
-
-	public static Date toDate(String date, String pattern) {
-		try {
-			date = date.replace("\"", "");
-			return new SimpleDateFormat(pattern).parse(date);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static boolean write(HttpServletResponse response, int status, String message) {
-		try {
-			response.setStatus(status);
-			response.setHeader("Content-type", "text/html;charset=UTF-8");
-			response.setCharacterEncoding("UTF-8");
-			response.getWriter().write(message);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	public static String getFirstDayOfWeek() {
-		Calendar c = Calendar.getInstance();
-		int day_of_week = c.get(Calendar.DAY_OF_WEEK) - 1;
-		if (day_of_week == 0)
-			day_of_week = 7;
-		c.add(Calendar.DATE, -day_of_week);
-		return DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
-	}
-
-	public static String getLastDayOfWeek() {
-		Calendar c = Calendar.getInstance();
-		int day_of_week = c.get(Calendar.DAY_OF_WEEK) - 1;
-		if (day_of_week == 0)
-			day_of_week = 7;
-		c.add(Calendar.DATE, -day_of_week + 6);
-		return DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
-	}
-
-	public static String getMonth() {
-		Calendar c = Calendar.getInstance();
-		c.add(Calendar.MONTH, -1);
-		return DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
-	}
-
-	public static String formatCurrency(double d, Locale l) {
-		String s = "";
-		try {
-			DecimalFormat nf = (DecimalFormat) NumberFormat.getCurrencyInstance(l);
-			// nf.applyPattern(pattern);
-			s = nf.format(d);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return s;
-	}
-
-	public static double cumulativeProbability(double[] array, double val) {
-		double index = 0;
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == val) {
-				index = (i + 1);
-				break;
-			}
-		}
-		return (index / array.length);
-	}
-
-
-	public static String formart(double number, int digits) {
-		NumberFormat nf = NumberFormat.getInstance();
-		nf.setMaximumFractionDigits(digits);
-		nf.setRoundingMode(RoundingMode.UP);
-		return nf.format(number);
-	}
-
-
-	public static Long getDiffDay(Date beginDate, Date endDate) {
-		return (endDate.getTime() - beginDate.getTime()) / (1000 * 24 * 60 * 60);
-	}
-
-	public static Long getDiffDay(long beginDate, long endDate) {
-		return (beginDate - endDate) / (1000 * 24 * 60 * 60);
-	}
-
-	public static String getCategory(String str) {
-		Matcher ma = Pattern.compile("[A-Z]+").matcher(str);
-		while(ma.find())
-			return ma.group();
-		return str;
-	}
-	
-	private static Calendar calendar;
-	public static int getWeekOfYear(Date date){
-		if(calendar==null){
-			calendar = Calendar.getInstance();  
-	        calendar.setFirstDayOfWeek(Calendar.SUNDAY);
-		}
-        calendar.setTime(date);
-        return calendar.get(Calendar.WEEK_OF_YEAR);
-	}
-	public static int getWeekOfYear(long millis){
-		if(calendar==null){
-			calendar = Calendar.getInstance();  
-	        calendar.setFirstDayOfWeek(Calendar.SUNDAY);
-		}
-        calendar.setTimeInMillis(millis);
-        return calendar.get(Calendar.WEEK_OF_YEAR);
 	}
 }
---
+++ src/test/java/JodaTimeTest.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2017/7/18.
+ */
+
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * @author lierl
+ * @create 2017-07-18 15:30
+ **/
+public class JodaTimeTest {
+
+	@Test
+	public void test1(){
+		DateTimeFormatter pattern = DateTimeFormat.forPattern("yyyy-MM-dd");
+
+		System.out.println(pattern.print(DateTime.now()));
+
+		DateTime dt = DateTime.now();
+
+		System.out.println(dt.toString("yyyy年MM月dd日"));
+
+		System.out.println(DateFormatUtils.format(new Date(),"yyyy-MM-dd"));
+	}
+}
